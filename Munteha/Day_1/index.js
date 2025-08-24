a962affaa94105c6b9101ddfab12945c79ff6bff
Samujjwal Patra <samujjwalpatra@gmail.com>
2025-08-24 20:42:27 +0530

    ADD SOME CHANGES

--- Munteha/Day_1/index.js
+++ Munteha/Day_1/index.js
@@ -1,7 +1,7 @@
-// const CountOddEvenArray = require('./src/8.Count_Odd_Even_Array.js');
+// const CountOE = require('./src/8.Count_Odd_Even_Array.js');
 // const a = [1, 4, 3, 8, 7];
-// CountOddEvenArray.Check(a);
+// CountOE.Check(a);
 
-const SumPrimeArray = require('./src/24.Sum_Prime_Array.js');
+const SumP = require('./src/24.Sum_Prime_Array.js');
 const a = [2, 3, 8, 4, 12, 10, 43, 56];
-SumPrimeArray.Check(a);
+SumP.Check(a);
